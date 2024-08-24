87c7b7284e3981aeda41e7f1c8d669123c0aa2ae
Aly Badawy <AlyBadawy@users.noreply.github.com>
2024-08-23 22:26:24 -0400

    I2C checker

---
+++ Drivers/VexUF/Inc/vexuf_i2c_checker.h
@@ -0,0 +1,17 @@
+/*
+ * vexuf_i2c_checker.h
+ *
+ *  Created on: Jul 26, 2024
+ *      Author: alybadawy
+ */
+
+#ifndef INC_VEXUF_I2C_CHECKER_H_
+#define INC_VEXUF_I2C_CHECKER_H_
+
+#include "vexuf_helpers.h"
+
+
+void I2C_Scan(uint8_t *foundDevices, uint8_t *foundDevicesCount);
+void I2C_ScanTest(void);
+
+#endif /* INC_VEXUF_I2C_CHECKER_H_ */
