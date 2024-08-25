d1017d4814d7273974349598baab2531367894a9
Aly Badawy <AlyBadawy@users.noreply.github.com>
2024-08-25 11:50:59 -0400

    Handle timers ticking

--- VexUF/Inc/vexuf_helpers.h
+++ VexUF/Inc/vexuf_helpers.h
@@ -8,24 +8,25 @@
 #ifndef VEXUF_VEXUF_HELPERS_H_
 #define VEXUF_VEXUF_HELPERS_H_
 
-#include "main.h"
-#include "string.h"
 #include <stdio.h>
 
-#define SERIAL_NUMBER_LENGTH		24
-#define CALLSIGN_LENGTH				20
+#include "main.h"
+#include "string.h"
+
+#define SERIAL_NUMBER_LENGTH 24
+#define CALLSIGN_LENGTH 20
 
 typedef struct {
-	uint16_t timer_1hz_ticked: 1;
-	uint16_t timer_10hz_ticked: 1;
-	uint16_t timer_0d1hz_ticked: 1;
-	uint16_t no_config_error: 1;
-	uint16_t sd_card_error: 1;
-	uint16_t sd_card_present: 1;
-	uint16_t ttlBuffered: 1;
-	uint16_t tncBuffered: 1;
-	uint16_t cdcBuffered: 1;
-	uint16_t reserved: 7;
+  uint16_t timer_1hz_ticked : 1;
+  uint16_t timer_10hz_ticked : 1;
+  uint16_t timer_0d1hz_ticked : 1;
+  uint16_t no_config_error : 1;
+  uint16_t sd_card_error : 1;
+  uint16_t sd_card_present : 1;
+  uint16_t ttlBuffered : 1;
+  uint16_t tncBuffered : 1;
+  uint16_t cdcBuffered : 1;
+  uint16_t reserved : 7;
 } VexufStatus;
 
 float cToF(float c);
--- VexUF/Src/vexuf_helpers.c
+++ VexUF/Src/vexuf_helpers.c
@@ -9,55 +9,42 @@
 
 static const char custom_base32_alphabet[] = "23456789ABCDEFGHJKLMNPQRSTUVWXYZ";
 char serialNumber[24];
+VexufStatus vexufStatus;
 
 void base32_encode(const uint8_t *data, size_t length, char *output);
 
 float cToF(float c) { return (c * (9.0 / 5.0)) + 32.0; }
-
 float fToC(float f) { return (f - 32.0) * (5.0 / 9.0); }
 
-uint16_t getSerialBytes(void) { return *(uint16_t *)0x08007FF0; }
+uint16_t getSerialBytes(void) { return *(uint16_t *)0x08007FFD; }
 
 void base32_encode(const uint8_t *data, size_t length, char *output) {
-  int index, bitbuffer, bits_in_buffer;
-  size_t i;
-  const uint8_t *current_byte;
+  int bitBuffer = 0, bitsInBuffer = 0;
+  size_t index = 0;
 
-  index = 0;
-  bitbuffer = 0;
-  bits_in_buffer = 0;
-  current_byte = data;
-
-  for (i = 0; i < length; i++) {
-    bitbuffer = (bitbuffer << 8) | *current_byte++;
-    bits_in_buffer += 8;
-
-    while (bits_in_buffer >= 5) {
+  for (size_t i = 0; i < length; ++i) {
+    bitBuffer = (bitBuffer << 8) | data[i];
+    bitsInBuffer += 8;
+    while (bitsInBuffer >= 5) {
       output[index++] =
-          custom_base32_alphabet[(bitbuffer >> (bits_in_buffer - 5)) & 0x1F];
-      bits_in_buffer -= 5;
+          custom_base32_alphabet[(bitBuffer >> (bitsInBuffer - 5)) & 0x1F];
+      bitsInBuffer -= 5;
     }
   }
-
-  if (bits_in_buffer > 0) {
+  if (bitsInBuffer > 0) {
     output[index++] =
-        custom_base32_alphabet[(bitbuffer << (5 - bits_in_buffer)) & 0x1F];
+        custom_base32_alphabet[(bitBuffer << (5 - bitsInBuffer)) & 0x1F];
   }
-
   output[index] = '\0';
 }
 
 void VexUF_GenerateSerialNumber() {
-  char serial[20];
-  uint32_t uid[3];
+  uint32_t uid[3] = {HAL_GetUIDw2(), HAL_GetUIDw1(), HAL_GetUIDw0()};
   uint8_t uid_bytes[12];
+  char serial[20];
 
-  uid[2] = HAL_GetUIDw0();
-  uid[1] = HAL_GetUIDw1();
-  uid[0] = HAL_GetUIDw2();
-
-  for (int i = 0; i < 3; i++) {
-    uid_bytes[4 * i] = (uid[i] >> 24) & 0xFF;
+  for (int i = 0; i < 3; ++i) {
+    uid_bytes[4 * i + 0] = (uid[i] >> 24) & 0xFF;
     uid_bytes[4 * i + 1] = (uid[i] >> 16) & 0xFF;
     uid_bytes[4 * i + 2] = (uid[i] >> 8) & 0xFF;
     uid_bytes[4 * i + 3] = uid[i] & 0xFF;
@@ -65,12 +52,9 @@ void VexUF_GenerateSerialNumber() {
 
   base32_encode(uid_bytes, 12, serial);
 
-  int i, j;
-  int length = strlen(serial);
-  for (i = 0, j = 0; i < length; i++) {
-    if (i > 0 && i % 5 == 0) {
-      serialNumber[j++] = '-';
-    }
+  int i = 0, j = 0;
+  for (i = 0; serial[i] != '\0'; ++i) {
+    if (i > 0 && i % 5 == 0) serialNumber[j++] = '-';
     serialNumber[j++] = serial[i];
   }
   serialNumber[j] = '\0';
