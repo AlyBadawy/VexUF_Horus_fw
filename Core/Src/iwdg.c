ba903be65faf4ef94d79a54430f2d089a30e97bf
Aly Badawy <AlyBadawy@users.noreply.github.com>
2024-08-25 16:41:04 -0400

    ADC and AVs driver (#37)

--- Core/Src/iwdg.c
+++ Core/Src/iwdg.c
@@ -4,8 +4,7 @@ IWDG_HandleTypeDef hiwdg;
 
 void MX_IWDG_Init(void) {
   hiwdg.Instance = IWDG;
-  hiwdg.Init.Prescaler = IWDG_PRESCALER_64;
-  hiwdg.Init.Reload = 2500;
+  hiwdg.Init.Prescaler = IWDG_PRESCALER_256;
+  hiwdg.Init.Reload = 625;
   if (HAL_IWDG_Init(&hiwdg) != HAL_OK) Error_Handler();
 }
-
